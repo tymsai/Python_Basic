f9ee32ee82a14446df2a61bf72073b44f4f20013
T.Y.M.SAI <tymsai6076@gmail.com>
2022-01-16 10:08:58 +0530

    Create slicing.py

---
+++ Strings/slicing.py
@@ -0,0 +1,6 @@
+#String slicing
+str="Strings in python has more features than in c++"
+print("string - Strings in python has more features than in c++")
+print(str[1:5]) #both values given
+print(str[:5]) #only 2nd value given
+print(str[0:]) #only 1st value given
