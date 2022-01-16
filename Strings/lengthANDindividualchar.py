7fc3831d7f2f88a3ce3ef72c4605cc413be0bf28
T.Y.M.SAI <tymsai6076@gmail.com>
2022-01-16 09:09:14 +0530

    Create lengthANDindividualchar.py

---
+++ Strings/lengthANDindividualchar.py
@@ -0,0 +1,10 @@
+#%%
+#Strings with len()
+str="Strings in python are easy than in c++"
+print("Strings in python are easy than in c++ \n", 
+      "Length of the  above string is -", len(str))
+
+#%%
+#Printing individual charecters from string
+str="Hello world"
+print("Hello world", str[6])
