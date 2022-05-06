8824681ca2bdaa37024b8d9cf217c0cf0e8e79c4
T.Y.M.SAI <tymsai6076@gmail.com>
2022-05-06 14:31:36 +0530

    Create NestedList.py

---
+++ List/NestedList.py
@@ -0,0 +1,7 @@
+#%%
+#Nested Lists
+numbers = [1, 2, 3, 4, 5,[ 6, 7, 8, 9]]
+letters = ['a', 'b', 'c', 'd', 'e']
+print("accessing the element in 2D list - ", numbers[5][1])
+letters+=numbers
+print("joining the 2 lists - ", letters)
