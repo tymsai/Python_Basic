cc17ff46ea551d2d3abbb1be7b1a413dfdb47d95
T.Y.M.SAI <tymsai6076@gmail.com>
2022-05-06 15:52:55 +0530

    Create main.py

---
+++ Tuples/main.py
@@ -0,0 +1,8 @@
+#%%
+#tuples
+numbers=((1, 2, 3), (4, 5, 6), (7, 8, 9))
+print("Accessing 2D tuple element - numbers[1][2] -> ", numbers[1][2])
+print(numbers[2:5])
+print(any(numbers))
+n=(1,2,3,4,5)
+print(n.count(5))
