b3d6fbe6ffb5ed625dc7a9544a73f5a64c00aae3
T.Y.M.SAI <tymsai6076@gmail.com>
2022-05-08 17:45:19 +0530

    Create MethodsInDictionary.py

---
+++ Dictionary/MethodsInDictionary.py
@@ -0,0 +1,10 @@
+#Dictionary Functions
+dict={1:'apple', 2:'ball', 3:'cake', 4:'dog', 5:'egg'}
+newdict=dict.copy()
+print(newdict)
+newdict.update({6:'fan'})
+print(newdict)
+newdict.pop(4)
+print(newdict)
+newdict.clear()
+print(newdict)
