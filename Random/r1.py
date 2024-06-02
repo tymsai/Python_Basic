164c177be8ab76e613abe1dda3d754849fa55b5c
T.Y.M.SAI <tymsai6076@gmail.com>
2024-06-02 11:14:25 +0530

    Create r1.py

---
+++ Random/r1.py
@@ -0,0 +1,17 @@
+"""
+reversing a string hardcoded
+"""
+arr=[1,22,3, 4,5,6]
+n=6
+d=3
+temp =0
+for i in arr:
+    temp=temp+1
+    if(temp>3):
+        print(i)
+temp =0
+for i in arr:
+    temp=temp+1
+    if(temp<=3):
+        print(i)
+      
